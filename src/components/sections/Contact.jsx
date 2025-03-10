14cc977a3b524ec6a3e1c1c8664d4986da37f29d
Nitish Kumar <pydnitish@gmail.com>
2025-03-10 23:54:45 +0530

    first commit

---
+++ src/components/sections/Contact.jsx
@@ -0,0 +1,128 @@
+import { useState } from "react";
+import { RevealOnScroll } from "../RevealOnScroll";
+import emailjs from "@emailjs/browser";
+import Alert from '@mui/material/Alert';
+
+export const Contact = () => {
+
+  const [formData, setFormData] = useState({});
+  const [isAlertClosed, setIsAlertClosed] = useState(false);
+  const successMessage = "Message Sent";
+  console.log(isAlertClosed);
+  console.log(setIsAlertClosed);
+
+  function handleChange(e){
+    console.log(e.target.id);
+    setFormData((prev)=>{
+      return {
+        ...prev,
+        [e.target.id]: e.target.value
+      }
+    });
+  }
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        e.target,
+        {publicKey:import.meta.env.VITE_PUBLIC_KEY}
+      )
+      .then(() => {
+        setIsAlertClosed(true);
+        setFormData({ user_name: "", user_email: "", message: "" });
+        setTimeout(()=>{
+          setIsAlertClosed(false);
+        },3000)
+      })
+      .catch(() => alert("Oops! Something went wrong. Please try again."));
+  };
+
+  return (
+    <section
+      id="contact"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+       {isAlertClosed && (
+        <div style={{
+          position: "fixed",
+          top: "0",
+          left: "50%",
+          transform: "translateX(-50%)",
+          width: "100%",
+          maxWidth: "400px",
+          zIndex: 1000,
+          textAlign: "center",
+          padding: "10px",
+        }}>
+          <Alert severity="success" onClose={() => setIsAlertClosed(false)}>
+            {successMessage}
+          </Alert>
+        </div>
+      )}
+      <RevealOnScroll>
+      
+        <div className="px-4 w-full min-w-[300px] md:w-[500px] sm:w-2/3 p-6">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Get In Touch
+          </h2>
+          <form className="space-y-6" onSubmit={handleSubmit} >
+          
+            <div className="relative">
+              <input
+                type="text"
+                id="user_name"
+                name="user_name"
+                required
+                value={formData.user_name}
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="Name..."
+                onChange={handleChange}
+               
+              />
+            </div>
+
+            <div className="relative">
+              <input
+                type="email"
+                id="user_email"
+                name="user_email"
+                required
+                value={formData.user_email}
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="example@gmail.com"
+                onChange={handleChange}
+               
+              />
+            </div>
+
+            <div className="relative">
+              <textarea
+                id="message"
+                name="message"
+                required
+                rows={5}
+                value={formData.message}
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="Your Message..."
+                onChange={handleChange}
+               
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+            >
+              Send Message
+            </button>
+          </form>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,251 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2"> CRM Analysis System</h3>
+              <p className="text-gray-400 mb-4">
+              The project involved analyzing sales data from Shell’s various food and beverage products across multiple locations and time zones. Data was sourced from LCMS in parquet format and stored in Azure Data Lake, processed via Azure Data Factory, and verified using Pyspark and SQL queries. The final reports were generated using Power BI.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Azure Data Lake", "Pyspark", "SQL", "Azure Data Factory", "Power Bi", "ETL Testing"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2"> Costco Wholesale Corp </h3>
+              <p className="text-gray-400 mb-4">
+              This project focused on migrating data from Oracle to Azure SQL Server and transforming Cognos reports into Power BI reports for Costco Wholesale. ETL processes ensured smooth data migration, and SQL queries verified the data integrity.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4"> 
+                {["SQL", "Azure SQL Server", "Power Bi", "ETL Testing", "Automation", "Reporting"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2"> Healthcare Insurance </h3>
+              <p className="text-gray-400 mb-4">
+              Humana’s data warehouse handled reporting for insurance premiums and claims. Data was received from various sources, transformed using business logic, and loaded into a central database. The project involved testing the ETL process and validating data in the target system.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4"> 
+                {["Oracle", "Informatica", "UNIX", "SQL", "ETL Testing", "Data Validation", "regression Testing"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2"> Cloud Platform</h3>
+              <p className="text-gray-400 mb-4">
+                Scalable cloud infrastructure management with real-time
+                monitoring and automated scaling.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">AI Analytics Dashboard</h3>
+              <p className="text-gray-400 mb-4">
+                ML-powered data visualization platform with predictive analytics
+                and interactive reports.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Python", "TensorFlow", "D3.js", "Flask"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">E-Commerce Web App</h3>
+              <p className="text-gray-400 mb-4">
+                Full-stack e-commerce with modern UI, secure payment
+                integration, and customizable product inventory.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Next.js", "TypeScript", "Stripe", "PostgreSQL"].map(
+                  (tech) => (
+                    <span
+                      key={tech}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Real-Time Chat App</h3>
+              <p className="text-gray-400 mb-4">
+                Scalable chat platform supporting real-time messaging, presence,
+                and group chat features.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Socket.IO", "Express", "React", "Redis"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
